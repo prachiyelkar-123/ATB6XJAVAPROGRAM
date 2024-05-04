1e61d80cce802e111759184490b76b90d4adb178
prachi <prachiyelkar096@gmail.com>
2024-05-04 18:15:23 +0530

    prachi

---
+++ src/EX_02052024/LAB17.java
@@ -0,0 +1,15 @@
+package EX_02052024;
+
+public class LAB17 {
+    public static void main(String[] args) {
+        int[] a = {27,25,26,52,36};
+        System.out.println(a.length);
+
+        for (int i = 0; i < a.length; i++) { // 0,1,2,3
+            System.out.println(a[i]);
+    }
+
+}
+
+
+}
