8c6f5ac574bf717b1e375b6ae7072d2c95302d6f
prachi <prachiyelkar096@gmail.com>
2024-05-25 18:48:03 +0530

    prachi

---
+++ src/EX_16052024/CodeExplaination.java
@@ -0,0 +1,27 @@
+package EX_16052024;
+
+public class CodeExplaination {
+    public static void main(String[] args) {
+        GrandFather g  = new Son();
+        g.home(); // What will be the output
+    }
+}
+class Son extends Father{
+    void home(){
+        System.out.println("3BHK");
+    }
+}
+
+
+class Father extends GrandFather{
+    void home(){
+        System.out.println("2BHK");
+    }
+}
+
+
+class GrandFather{
+    void home(){
+        System.out.println("1BHK");
+    }
+}
